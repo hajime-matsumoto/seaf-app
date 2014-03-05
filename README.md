dfbcc8161ac625fae1e1454216538c5242db4195
Hajime MATSUMOTO <mail@hazime.org>
2014-03-05 00:27:45 +0000

    Create Seaf App

---
+++ README.md
@@ -0,0 +1,17 @@
+Seaf Appライブラリ
+======================
+
+アプリケーションの基礎組になるライブラリ
+
+主なクラス
+---------------------
+
+* Seaf\App
+* Seaf\App\Component\Request\Request
+* Seaf\App\Component\Router
+* Seaf\App\Component\Response
+
+
+依存
+---------------------
+* seaf-yaml
---
+++ src/App/Component/Response/Response.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Seaf: Simple Easy Acceptable micro-framework.
+ *
+ * @author HAjime MATSUMOTO <mail@hazime.org>
+ * @copyright Copyright (c) 2014, Seaf
+ * @license   MIT, http://seaf.hazime.org
+ */
+
+namespace Seaf\Component\Response;
+
+/**
+ * レスポンスコンポーネント
+ */
+class Response implements ResponseIF
+{
+}
+
+/* vim: set expandtab ts=4 sw=4 sts=4: et*/
